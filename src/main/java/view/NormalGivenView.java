a88fc9f0ef8c8185a44a925acf54bfd3b94a6610
Yue Zhao <1791963007@qq.com>
2024-11-17 14:21:50 -0500

    1

---
+++ src/main/java/view/NormalGivenView.java
@@ -0,0 +1,146 @@
+package view;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class NormalGivenView extends JFrame {
+
+    private JPanel gameArea;
+    private JPanel sideArea;
+    private final int widowWidth = 515;
+    private final int widowHeight = 635;
+    private final int gameAreaWidth = 300;
+    private final int gameAreaHeight = 600;
+    private final int sideAreaWidth = 200;
+    private final int sideAreaHeight = 400;
+    // private final ViewModel viewModel;
+    private final int squareSize = 30;
+    private final int margin = 5;
+
+    public NormalGivenView() {
+        // Set the title and default close operation
+        setTitle("Game Screen Layout");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(widowWidth, widowHeight);
+
+        // Set the layout for the main frame
+        setLayout(new BorderLayout());
+
+// Create a topPanel to hold gameArea and sideArea
+        JPanel topPanel = new JPanel(new BorderLayout());
+
+// Create a left-aligned panel to hold gameArea
+        JPanel gameAreaWrapper = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        final Border redline = BorderFactory.createLineBorder(Color.red);
+        gameArea = new JPanel();
+        gameArea.setBackground(Color.BLACK);
+        gameArea.setPreferredSize(new Dimension(gameAreaWidth, gameAreaHeight));
+        gameArea.setBorder(redline);
+        gameAreaWrapper.add(gameArea);  // Add gameArea to the left-aligned wrapper
+        topPanel.add(gameAreaWrapper, BorderLayout.WEST);  // Add wrapper to topPanel
+
+// Create a right-aligned panel to hold sideArea
+        JPanel sideAreaWrapper = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+        final Border yellowline = BorderFactory.createLineBorder(Color.yellow);
+        sideArea = new JPanel();
+        sideArea.setBackground(Color.GRAY);
+        sideArea.setPreferredSize(new Dimension(sideAreaWidth, sideAreaHeight));
+        sideArea.setBorder(yellowline);
+        sideAreaWrapper.add(sideArea);  // Add sideArea to the right-aligned wrapper
+        topPanel.add(sideAreaWrapper, BorderLayout.EAST);  // Add wrapper to topPanel
+
+// Add topPanel to the main frame at the top (NORTH)
+        add(topPanel, BorderLayout.NORTH);
+
+        setupKeyBindings();
+
+        // Make the frame visible
+        setVisible(true);
+    }
+
+    public void setupKeyBindings() {
+        /*
+        setup listeners for W A S D
+        */
+        final InputMap inputMap = gameArea.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        final ActionMap actionMap = gameArea.getActionMap();
+
+        inputMap.put(KeyStroke.getKeyStroke("W"), "keyWPressed");
+        actionMap.put("keyWPressed", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when "B" is pressed
+                // NormalGivenController executes based on this key
+                System.out.println("W");
+            }
+        });
+
+        inputMap.put(KeyStroke.getKeyStroke("A"), "keyAPressed");
+        actionMap.put("keyAPressed", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when "A" is pressed
+                // NormalGivenController executes based on this key
+                System.out.println("A");
+            }
+        });
+
+        inputMap.put(KeyStroke.getKeyStroke("S"), "keySPressed");
+        actionMap.put("keySPressed", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when "S" is pressed
+                // NormalGivenController executes based on this key
+                System.out.println("S");
+            }
+        });
+
+        inputMap.put(KeyStroke.getKeyStroke("D"), "keyDPressed");
+        actionMap.put("keyDPressed", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when "D" is pressed
+                // NormalGivenController executes based on this key
+                System.out.println("D");
+                drawTest();
+            }
+        });
+    }
+    /*
+    private void draw(ViewModel v) {
+        currentMap = v.getMap();
+        for (int i = 0; i < currentMap.length(); i++) {
+
+            for (int j = 0; j < currentMap[0].length; j++) {
+                if (currentMap[i][j] == 1) {
+                    gameArea.add(squareFactory(squareSize * i, squareSize * j, squareSize));
+                }
+            }
+        }
+        gameArea.repaint();
+    }
+     */
+    private void draw() {
+        int[][] currentMap = v.getMap();
+        for (int i = 0; i < currentMap.length; i++) {
+
+            for (int j = 0; j < currentMap[0].length; j++) {
+                if (currentMap[i][j] == 1) {
+                    gameArea.add(squareFactory(margin + squareSize * j,
+                            squareSize * i - margin, squareSize));
+                }
+            }
+        }
+        gameArea.repaint();
+    }
+
+    private JLabel squareFactory(int xPosition, int yPosition, int size) {
+        final JLabel square = new JLabel();
+        square.setOpaque(true);
+        square.setBackground(Color.BLUE);
+        square.setBounds(xPosition, yPosition, size, size); // Initial position and size
+        square.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+        return square;
+    }
